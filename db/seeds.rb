2fc175cb01f05d5b3aaaa35a317902088b586285
jsabath <justin.sabath@aol.com>
2020-05-06 09:02:59 -0600

    Seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# Event.destroy_all
+# Calendar.destroy_all
+# User.destroy_all
+
+event1 = Event.create(event: "Dentist Appt", location: "Cool Smilez", start_time: "2020-05-08 01:30", end_time: "2020-05-05 02:30:")
+event2 = Event.create(event: "Teach", location: "Flatiron Campus", start_time: "2020-05-10 07:30", end_time: "2020-05-10 10:00")
+event3 = Event.create(event: "Oil Change", location: "Jiffy Lube", start_time: "2020-05-15 03:00", end_time: "2020-05-15 04:15")
+
+#
+# calendar1 = Calendar.create()
+user1 = User.create(username: "Ahmed")
